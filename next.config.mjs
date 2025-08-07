18fa8b38a9c8b710e6948e8dbd442528670b6ba6
firas <firas@evidentia.ai>
2025-08-06 23:03:47 -0700

    fix basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const repoName = 'evidentiaai.github.io'
 const nextConfig = {
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   eslint: {
     ignoreDuringBuilds: true,
   },
